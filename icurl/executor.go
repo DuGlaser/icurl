af471f92cdd4c7ca596593d2a6d9f38d27f5afe3
DuGlaser <duglaser0618@gmail.com>
2020-11-19 22:49:21 +0900

    :truck: Move the Executor

---
+++ icurl/executor.go
@@ -0,0 +1,49 @@
+package icurl
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func Excutor(t string) {
+	t = strings.TrimSpace(t)
+
+	if t == "" {
+		return
+	}
+
+	if t == "exit" {
+		os.Exit(0)
+		return
+	}
+
+	arg := strings.Split(t, " ")
+
+	for i, a := range arg {
+		if a == "--editor" {
+			content, _ := Launch()
+			var buf bytes.Buffer
+			json.Compac(&buf, content)
+
+			arg[i] = fmt.Sprintf("\"%s\"", buf.String())
+		}
+	}
+
+	fmt.Print("cmd: curl ")
+	for _, a := range arg {
+		fmt.Printf("%s ", a)
+	}
+	fmt.Println()
+	fmt.Println()
+
+	cmd := exec.Command("curl", arg...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+	return
+}
--- main.go
+++ main.go
@@ -1,44 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
-
 	"github.com/DuGlaser/icurl/icurl"
 	"github.com/c-bata/go-prompt"
 )
-
-func executor(t string) {
-	arg := strings.Split(t, " ")
-
-	for i, a := range arg {
-		if a == "--editor" {
-			content, _ := icurl.Launch()
-			var buf bytes.Buffer
-			json.Compac(&buf, content)
-
-			arg[i] = fmt.Sprintf("\"%s\"", buf.String())
-		}
-	}
-
-	fmt.Print("cmd: curl ")
-	for _, a := range arg {
-		fmt.Printf("%s ", a)
-	}
-	fmt.Println()
-	fmt.Println()
-
-	cmd := exec.Command("curl", arg...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	return
-}
 
 func main() {
 	c, err := icurl.NewCompleter()
@@ -47,7 +12,7 @@ func main() {
 	}
 
 	p := prompt.New(
-		executor,
+		icurl.Excutor,
 		c.Complete,
 		prompt.OptionPrefix(">>> curl "),
 		prompt.OptionTitle("icurl"),
