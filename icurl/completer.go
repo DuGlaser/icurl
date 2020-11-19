4e83cb5a690aa0003f49293ac292551d8c26fd4e
DuGlaser <duglaser0618@gmail.com>
2020-11-19 22:26:51 +0900

    :sparkles: Add the process to switch complementation word

--- icurl/completer.go
+++ icurl/completer.go
@@ -1,6 +1,10 @@
 package icurl
 
-import "github.com/c-bata/go-prompt"
+import (
+	"strings"
+
+	"github.com/c-bata/go-prompt"
+)
 
 func NewCompleter() (*Completer, error) {
 	return &Completer{}, nil
@@ -9,19 +13,61 @@ func NewCompleter() (*Completer, error) {
 type Completer struct{}
 
 func (c *Completer) Complete(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "-X GET", Description: "HTTP GET method"},
-		{Text: "-X POST", Description: "HTTP POST method"},
-		{Text: "-X DELETE", Description: "HTTP DELETE method"},
-		{Text: "-X PATCH", Description: "HTTP PATCH method"},
-		{Text: "-X PUT", Description: "HTTP PUT method"},
-		{Text: "-d", Description: "HTTP POST data"},
-		{Text: "-I", Description: "Show document info only"},
-		{Text: "-v", Description: "Make the operation more talkative"},
-		{Text: "-h", Description: "This help text"},
-		{Text: "-H", Description: "Pass custom header(s) to server"},
-		{Text: "\"Content-Type: application/json\""},
-		{Text: "--editor", Description: "Open editor"},
+	if d.TextBeforeCursor() == "" {
+		return []prompt.Suggest{}
 	}
-	return prompt.FilterFuzzy(s, d.GetWordBeforeCursor(), true)
+
+	args := strings.Split(d.TextBeforeCursor(), " ")
+	w := d.GetWordBeforeCursor()
+
+	for i := range args {
+		if args[i] == "|" {
+			return []prompt.Suggest{}
+		}
+	}
+
+	if strings.HasPrefix(w, "--") {
+		return OptionCompleter(d)
+	}
+
+	if strings.HasPrefix(w, "-") {
+		return ShortOptionCompleter(d)
+	}
+
+	if s, f := c.completeOptionArguments(d); f {
+		return s
+	}
+
+	return []prompt.Suggest{}
+}
+
+func getPreviousOption(d prompt.Document) (option string, found bool) {
+	args := strings.Split(d.TextBeforeCursor(), " ")
+	l := len(args)
+	if l >= 2 {
+		option = args[l-2]
+	}
+	if strings.HasPrefix(option, "-") {
+		return option, true
+	}
+
+	return "", false
+}
+
+func (c *Completer) completeOptionArguments(d prompt.Document) ([]prompt.Suggest, bool) {
+	option, found := getPreviousOption(d)
+
+	if !found {
+		return []prompt.Suggest{}, false
+	}
+
+	switch option {
+	case "-X", "--request":
+		return HttpMethodArguments(d), true
+
+	case "-d", "--data":
+		return []prompt.Suggest{{Text: "--editor", Description: "Open the editor"}}, true
+	}
+
+	return []prompt.Suggest{}, false
 }
